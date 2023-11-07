23c0df161f329ec2974e3c6162b1097be6ff2a07
danielplebani <danielpleba03@gmail.com>
2023-11-07 17:30:39 +0100

    add a name in the seeder

--- database/seeders/ComicsSeeder.php
+++ database/seeders/ComicsSeeder.php
@@ -14,6 +14,7 @@ class ComicsSeeder extends Seeder
     public function run(): void
     {
         $comic = new Comic();
+        $comic->name = " NOME COMICS 1";
         $comic->save();
     }
 }
